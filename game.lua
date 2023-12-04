364e129b70504413940c6c50ca1f6be20f3ade05
litten-hug <90008250+litten-hug@users.noreply.github.com>
2023-12-04 08:07:16 -0800

    Idle update
    Finallised the idle animation
    (That's it)

--- game.lua
+++ game.lua
@@ -1,45 +1,56 @@
 function love.load()
+  idleCount = 1
   p1_idleFrames = {}
   
-  for i=1,5 do
-      table.insert(p1_idleFrames, love.graphics.newImage("Sprites/Animations/p1_idle_Frame" .. i .. ".png"))
+  for i = 1, 11 do
+    table.insert(p1_idleFrames, love.graphics.newImage("Sprites/Animations/p1_idle_Frame" .. i .. ".png"))
   end
+
   currentFrame = 1
+  showHelp = true
   
   p2 = {
     image = love.graphics.newImage('sprites/P2_Left.png'),
     x = 350,
     y = 50
   }
+
   p1 = {
     image = love.graphics.newImage('sprites/P1_Right.png'),
     x = 100,
     y = 50
   }
-  showHelp = true
 end
 
 function love.update(dt)
-  currentFrame = currentFrame + dt * 4
-  if currentFrame >= 6 then
-    currentFrame = 1
+  idleCount = idleCount + dt
+  if idleCount >= 4 then
+    currentFrame = currentFrame + dt * 5
+    if currentFrame >= 11 then
+      currentFrame = 1
+      idleCount = 1
+    end
   end
   LuaReload.Monitor()
   if love.keyboard.isDown("right") then
     p1.x = p1.x + 300 * dt
     showHelp = false
+    idleCount = 1
   end
   if love.keyboard.isDown("left") then
     p1.x = p1.x - 300 * dt
     showHelp = false
+    idleCount = 1
   end
   if love.keyboard.isDown("down") then
     p1.y = p1.y + 300 * dt
     showHelp = false
+    idleCount = 1
   end
   if love.keyboard.isDown("up") then
     p1.y = p1.y - 300 *dt
     showHelp = false
+    idleCount = 1
   end
 end
   
@@ -51,7 +62,11 @@ function love.draw()
     love.graphics.print("Arrow keys to move.", 300, 50)
   end
   love.graphics.draw(p2.image, p2.x ,p2.y)
-  love.graphics.draw(p1_idleFrames[math.floor(currentFrame)], p1.x, p1.y)
+  if idleCount >= 4 then
+    love.graphics.draw(p1_idleFrames[math.floor(currentFrame)], p1.x, p1.y)
+  else
+    love.graphics.draw(p1.image, p1.x, p1.y)
+  end  
   if checkCollision(p1, p2) == true then
     love.graphics.setColor(1, 1, 1)
     love.graphics.print("COLLISION DETECTED", 100, 50)
